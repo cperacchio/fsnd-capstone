702a2a6a936ce494314d3a24bc6dd06d5c65ae22
Claire P <claire.peracchio@gmail.com>
2020-10-04 21:23:09 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import setup_db
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
