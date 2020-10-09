fec41a2f2e60d64c3bf7ab8fff54653b6a2c1779
Claire P <claire.peracchio@gmail.com>
2020-10-09 13:13:33 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, String, create_engine
-from flask_sqlalchemy import SQLAlchemy
-import json
 import os
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, sessionmaker
+import json, sys
 
 os.environ['DB_CONN'] = 'postgres://claireperacchio@localhost:5432/capstone'
 database_path = os.environ['DB_CONN']
@@ -21,23 +22,80 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Classes
-Have title and release year
+Models
 '''
+
+actor_movie = db.Table('actor_movie',
+    Column('id', db.Integer, primary_key=True),
+    Column('movie_id', db.Integer, db.ForeignKey('movie.id')),
+    Column('actor_id', db.Integer, db.ForeignKey('actor.id'))
+)
+
+director_movie = db.Table('director_movie',
+    Column('id', db.Integer, primary_key=True),
+    Column('movie_id', db.Integer, db.ForeignKey('movie.id')),
+    Column('director_id', db.Integer, db.ForeignKey('director.id'))
+)
+
 class Movie(db.Model):  
   __tablename__ = 'Movie'
 
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(120), nullable=False)
+  name = db.Column(db.String(120), nullable=False)
   release_year = db.Column(db.Integer, nullable=False)
   rating = db.Column(db.String(120), nullable=False)
-  run_time = db.Column(db.Integer, nullable=False)
   box_office = db.Column(db.Integer, nullable=False)
-  actors = db.relationship('Actor', backref='movie')
-  director = db.relationship('Director', backref='movie')
+  image_link = db.Column(db.String(500))
+  cast = db.relationship('Actor', secondary=actor_movie, back_populates='movies', cascade='save-update, merge', lazy='dynamic')
+  director = db.relationship('Director', secondary=director_movie, back_populates='movies', cascade='save-update, merge', lazy='dynamic')
 
-  def __repr__(self):
-    return f'<Movie {self.id} {self.title}>'
+  def __init__(self, name, release_year, rating, box_office, image_link, director):
+        self.name = name
+        self.release_year = release_year
+        self.rating = rating
+        self.box_office = box_office
+        self.image_link = image_link
+        self.director = director
+
+  def insert(self):
+    try:
+      db.session.add(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+    print(sys.exc_info())
+  
+  def update(self):
+    try:
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+  def delete(self):
+    try:
+      db.session.delete(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+
+  def format(self):
+      return {
+          'name': self.name,
+          'id': self.id,
+          'release_year': self.release_year,
+          'cast': [actor.format_no_movies() for actor in self.cast],
+          'director': self.director
+      }
+
+  def format_with_no_cast(self):
+      return {
+          'name': self.name,
+          'release_year': self.release_year,
+          'director': self.director
+      }
 
 class Actor(db.Model):
   __tablename__ = 'Actor'
@@ -45,11 +103,54 @@ class Actor(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(120), nullable=False)
   age = db.Column(db.Integer, nullable=False)
-  gender = db.Column(db.Char(1), nullable=False)
-  movie_id = Column(Integer, ForeignKey('movie.id'))
+  gender = db.Column(db.String(1), nullable=False)
+  image_link = db.Column(db.String(500))
+  movies = db.relationship('Movie', secondary=actor_movie, lazy='joined')
 
-  def __repr__(self):
-    return f'<Actor {self.id} {self.name}>'
+  def __init__(self, name, age, gender, image_link):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.image_link = image_link
+        
+  def insert(self):
+    try:
+      db.session.add(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+  
+  def update(self):
+    try:
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+  def delete(self):
+    try:
+      db.session.delete(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+  def format(self):
+      return {
+          'name': self.name,
+          'id': self.id,
+          'age': self.age,
+          'gender': self.gender,
+          'movies': [movie.format_with_no_cast() for movie in self.movies]
+      }
+
+  def format_no_movies(self):
+      return {
+          'name': self.name,
+          'age': self.age,
+          'gender': self.gender,
+      }
 
 class Director(db.Model):
   __tablename__ = 'Director'
@@ -57,8 +158,51 @@ class Director(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(120), nullable=False)
   age = db.Column(db.Integer, nullable=False)
-  gender = db.Column(db.Char(1), nullable=False)
-  movie_id = Column(Integer, ForeignKey('movie.id'))
+  gender = db.Column(db.String(1), nullable=False)
+  image_link = db.Column(db.String(500))
+  movies = db.relationship('Movie', secondary=director_movie, lazy='joined')
 
-  def __repr__(self):
-    return f'<Director {self.id} {self.name}>'
+  def __init__(self, name, age, gender, image_link):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.image_link = image_link
+        
+  def insert(self):
+    try:
+      db.session.add(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+  
+  def update(self):
+    try:
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+  def delete(self):
+    try:
+      db.session.delete(self)
+      db.session.commit()
+    except: 
+      db.session.rollback()
+      print(sys.exc_info())
+
+  def format(self):
+      return {
+          'name': self.name,
+          'id': self.id,
+          'age': self.age,
+          'gender': self.gender,
+          'movies': [movie.format_with_no_cast() for movie in self.movies]
+      }
+
+  def format_no_movies(self):
+      return {
+          'name': self.name,
+          'age': self.age,
+          'gender': self.gender,
+      }
