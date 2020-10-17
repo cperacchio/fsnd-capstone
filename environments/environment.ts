018eaa350304d91f0fbad6d06338ea162fc128bc
Claire P <claire.peracchio@gmail.com>
2020-10-17 13:53:01 -0400

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,13 @@
+# set up environment variables
+
+export const environment = {
+	production: false,
+	apiServerUrl: 'http://127.0.0.1:5000/',
+	auth0: {
+		url: 'fsnd79',
+		audience: 'casting',
+		clientId: '2FaJjQSAtsiLqHMEfNlS0ThYQ6Oyuh9c',
+		callbackURL: 'http://localhost:8100'
+	}
+
+}
