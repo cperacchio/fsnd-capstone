44658d80dfe6735685f4efa9681522ff18a11d55
Claire Peracchio <claire.peracchio@gmail.com>
2023-02-02 08:19:41 -0800

    Update models.py
    fix heroku deploy issue

--- models.py
+++ models.py
@@ -19,7 +19,10 @@ def setup_db(app, database_path=database_path):
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 	db.app = app
 	db.init_app(app)
-	db.create_all()
+	try:
+		db.create_all()
+	except:
+		print("error setting up db")
 
 '''
 Models
